e15e0da2963b173a6fb9611e0f58432f3a183075
«pgm-gauttijt» <gauttijt@student.arteveldehs.be>
2022-12-08 11:06:15 +0100

    Update for updating the core

--- update_addon.py
+++ update_addon.py
@@ -14,7 +14,7 @@ from requests import post
 
 url = "http://debugcontroller.homate.ml/api/services/hassio/addon_update"
 headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2NjcwMDJiNTI0ZDA0ZjY5OTFkZDk2NTQ5YTlkYjVlZiIsImlhdCI6MTY3MDQyMDI5MCwiZXhwIjoxOTg1NzgwMjkwfQ.ERTpyFAomtsTxjc1ngC0swEEBigPQWvzyFJORetphqw"}
-data = {"addon": "a0d7b954_vscode"}
+data = {"addon": "45df7312_zigbee2mqtt"}
 
 response = post(url, headers=headers, json=data)
 print(response.text)
