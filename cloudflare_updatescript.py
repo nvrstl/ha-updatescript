0e66ed393ae643407de691791e7e645c9ece0750
«pgm-gauttijt» <gauttijt@student.arteveldehs.be>
2022-12-09 15:47:29 +0100

    Added cloudflare and started testing the NodeRED update

---
+++ cloudflare_updatescript.py
@@ -0,0 +1,45 @@
+import csv
+import time
+from requests import post
+import requests
+
+
+#DEFINE USER CONSTANTS
+update_addon_string = "api/services/hassio/addon_update"
+data = {"addon": "9074a9fa_cloudflared"}
+
+print ("Script has been started!")
+#OPEN CSV FILE AND SKIP HEADER
+with open("server_db.csv", 'r') as file:
+  csvreader = csv.reader(file)
+  next(csvreader,None)
+
+#ITERATE OVER THE CSV LIST (# of iterations = # of rows)
+  for row in csvreader:
+    # print(row)    
+    # print(row[1])
+    url = row[2] + update_addon_string
+    bearer_token = row[1]
+    headers = {"Authorization": "Bearer {}".format(bearer_token)}
+    # print(headers)
+    # print(url)
+    # print(data) 
+    print("----------------------------------------")
+    try:
+        response = post(url, headers=headers, json=data)
+        response.raise_for_status()
+        time.sleep(1)
+        print("INF: Updating Cloudflare on server: {}".format(url)+" ...")
+        time.sleep(2)
+        print("INF: Cloudflare on {}".format(url)+"has been sucessfully updated!")
+        time.sleep(4)
+        print(response.status_code)
+    except requests.exceptions.HTTPError as err:
+        print("WAR: Cloudflare on server {} has already been updated".format(url))
+        time.sleep(4)
+    except requests.exceptions.InvalidSchema as errTimeout:
+        print("ERR: Can't reach Server {}".format(url))
+        time.sleep(4)
+
+    # print(hassio_url)
+    # print(bearer_token)
--- update_addon.py
+++ update_addon.py
@@ -12,9 +12,10 @@
 
 from requests import post
 
-url = "http://debugcontroller.homate.ml/api/services/hassio/addon_update"
-headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2NjcwMDJiNTI0ZDA0ZjY5OTFkZDk2NTQ5YTlkYjVlZiIsImlhdCI6MTY3MDQyMDI5MCwiZXhwIjoxOTg1NzgwMjkwfQ.ERTpyFAomtsTxjc1ngC0swEEBigPQWvzyFJORetphqw"}
-data = {"addon": "45df7312_zigbee2mqtt"}
+url = "https://autreve-m1xh9v.homate.ml/api/services/hassio/addon_update"
+headers = {"Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJmN2M1ZTIxZDJkMGQ0Y2VjYTNjZjEyNWFmOTBhMDI1ZCIsImlhdCI6MTY3MDU5MzUzMSwiZXhwIjoxOTg1OTUzNTMxfQ.l0aj70FaqdSAi1VWyUIDGLLc1VnLcuzNy__MmbjYKrY"}
+data = {"addon": "9074a9fa_cloudflared"}
 
 response = post(url, headers=headers, json=data)
-print(response.text)
+response.raise_for_status()
+print(response.status_code)
