8b832f1db63a2cd97acd3c47b88b45c10f47a8c9
«pgm-gauttijt» <gauttijt@student.arteveldehs.be>
2022-12-09 13:53:56 +0100

    First succesful test

--- updatescript.py
+++ updatescript.py
@@ -7,22 +7,24 @@ update_core_string = "/api/services/update/install"
 data = {"entity_id": "update.home_assistant_core_update"}
 
 #OPEN CSV FILE AND SKIP HEADER
-with open("test_db.csv", 'r') as file:
+with open("server_db.csv", 'r') as file:
   csvreader = csv.reader(file)
   next(csvreader,None)
-  
+
 #ITERATE OVER THE CSV LIST (# of iterations = # of rows)
   for row in csvreader:
     # print(row)
     # print(row[1])
-    hassio_url = row[2] + update_core_string
+    url = row[2] + update_core_string
     bearer_token = row[1]
     headers = {"Authorization": "{}".format(bearer_token)}
     print(headers)
-    print(hassio_url)
+    print(url)
     print(data)
     print("----------------------------------------")
-    # response = post(hassio_url, headers=headers, json=data)
+    response = post(url, headers=headers, json=data)
+    print("----------------------------------------")
+    print(response.text)
     time.sleep(8)
     
     # print(hassio_url)
