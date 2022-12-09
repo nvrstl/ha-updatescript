b2b7d3bfa7568959701c72db8c38d2c343d7ada2
«pgm-gauttijt» <gauttijt@student.arteveldehs.be>
2022-12-09 11:37:02 +0100

    Added readout of columns

--- updatescript.py
+++ updatescript.py
@@ -3,8 +3,14 @@ from requests import post
 
 with open("test_db.csv", 'r') as file:
   csvreader = csv.reader(file)
-
+  next(csvreader,None)
   
   #Iterate over the csv list (# of rows = # of iterations)
   for row in csvreader:
-    print(row[0].split(",")[0])
+    print(row)
+    print(row[1])
+
+    hassio_url = row[2]
+    bearer_token = row[1]
+    print(hassio_url)
+    print(bearer_token)
