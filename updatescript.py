3a0613344e26fa0e93e77f38866243e944566a8b
«pgm-gauttijt» <gauttijt@student.arteveldehs.be>
2022-12-09 13:39:59 +0100

    Combined the CSV.READOUT and the UPDATE script, tested without response

--- updatescript.py
+++ updatescript.py
@@ -1,16 +1,29 @@
 import csv
+import time
 from requests import post
 
+#DEFINE USER CONSTANTS
+update_core_string = "/api/services/update/install"
+data = {"entity_id": "update.home_assistant_core_update"}
+
+#OPEN CSV FILE AND SKIP HEADER
 with open("test_db.csv", 'r') as file:
   csvreader = csv.reader(file)
   next(csvreader,None)
   
-  #Iterate over the csv list (# of rows = # of iterations)
+#ITERATE OVER THE CSV LIST (# of iterations = # of rows)
   for row in csvreader:
-    print(row)
-    print(row[1])
-
-    hassio_url = row[2]
+    # print(row)
+    # print(row[1])
+    hassio_url = row[2] + update_core_string
     bearer_token = row[1]
+    headers = {"Authorization": "{}".format(bearer_token)}
+    print(headers)
     print(hassio_url)
-    print(bearer_token)
+    print(data)
+    print("----------------------------------------")
+    # response = post(hassio_url, headers=headers, json=data)
+    time.sleep(8)
+    
+    # print(hassio_url)
+    # print(bearer_token)
