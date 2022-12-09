2a4d2303e9fe0ad19c71f5cb3a36f2e1f51dbe04
«pgm-gauttijt» <gauttijt@student.arteveldehs.be>
2022-12-09 14:31:56 +0100

    Updated the error handling

--- updatescript.py
+++ updatescript.py
@@ -1,6 +1,7 @@
 import csv
 import time
 from requests import post
+import requests
 
 
 #DEFINE USER CONSTANTS
@@ -8,6 +9,7 @@ update_core_string = "/api/services/update/install"
 data = {"entity_id": "update.home_assistant_core_update"}
 i = 0 
 
+print ("Script has been started!")
 #OPEN CSV FILE AND SKIP HEADER
 with open("server_db.csv", 'r') as file:
   csvreader = csv.reader(file)
@@ -21,14 +23,19 @@ with open("server_db.csv", 'r') as file:
     url = row[2] + update_core_string
     bearer_token = row[1]
     headers = {"Authorization": "{}".format(bearer_token)}
-    print(headers)
-    print(url)
-    print(data)
+    # print(headers)
+    # print(url)
+    # print(data) 
     print("----------------------------------------")
-    response = post(url, headers=headers, json=data)
-    print("Updating row {}".format(i)+"...")
-    print(response.text)
-    time.sleep(8)
-    
+    try:
+        response = post(url, headers=headers, json=data)
+        response.raise_for_status()
+        print("Updating server: {}".format(url)+"...")
+        time.sleep(2)
+        print("Server {}".format(url)+"has been sucessfully updated!")
+        time.sleep(4)
+    except requests.exceptions.HTTPError as err:
+        print("ERR: Server {} has already been updated".format(url))
+        time.sleep(4)
     # print(hassio_url)
     # print(bearer_token)
